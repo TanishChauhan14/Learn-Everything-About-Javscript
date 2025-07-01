59084d809128c415b86774c74dacd784657eb8d0
Tanish chauhan <161298899+TanishChauhan14@users.noreply.github.com>
2025-07-01 04:39:27 +0000

    Add detailed comments explaining variable declarations and scopes

--- Learn Everything About Js/script.js
+++ Learn Everything About Js/script.js
@@ -6,3 +6,5 @@ a= 15; // Initialization
 var x = 12; // Declare and Initialization
 
 // Var  :- 1. Window mein add hota hai. 2.Function scoped hota hai.  3.Redeclare kr skte hai.
+// Let :- 1. Reclare nahi kr skte. 2.Block scoped hota hai. 3.Window mein add nahi hota.
+// Const :- 1. Reassign nahi kr skte. 2.Block scoped hota hai. 3.Window mein add nahi hota.
