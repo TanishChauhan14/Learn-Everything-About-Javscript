53cbd44707ec58124b7d50ca0ed77dc5c3b3557a
Tanish chauhan <161298899+TanishChauhan14@users.noreply.github.com>
2025-07-01 04:20:55 +0000

    First Commit

---
+++ Learn Everything About Js/script.js
@@ -0,0 +1,8 @@
+// Var Let Const
+// Declaration and Initialization
+
+let a ; // Declare karna
+a= 15; // Initialization
+var x = 12; // Declare and Initialization
+
+// Var  :- 1. Window mein add hota hai. 2.Function scoped hota hai.  3.Redeclare kr skte hai.
